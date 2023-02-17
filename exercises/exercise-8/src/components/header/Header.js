0ffac02633668a56519012a9b20ecec14dedd1a6
Sagun15 <1rn19cs123.sagun@gmail.com>
2023-02-17 12:20:34 +0530

    Make changes to padding and items

--- exercises/exercise-8/src/components/header/Header.js
+++ exercises/exercise-8/src/components/header/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <section className="header-section">
       <div className="header">
-        <h1 className="header__title">Item Lister</h1>
+        <p className="header__title">Item Lister</p>
         <input
           className="header__search-bar"
           type="search"
